2b97e7eb6445abd8406b43bd4799ae5e6cc00d20
HongzCloud <jihong21511@gmail.com>
2024-01-06 23:31:15 +0900

    Initial Commit

---
+++ AppLocker/AppLockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppLockerApp.swift
+//  AppLocker
+//
+//  Created by 오킹 on 2024/01/06.
+//
+
+import SwiftUI
+
+@main
+struct AppLockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppLocker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppLocker
+//
+//  Created by 오킹 on 2024/01/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
