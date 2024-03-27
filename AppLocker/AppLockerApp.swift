75b85ea927c3317233406989775d30a0dc06bb38
HongzCloud <jihong21511@gmail.com>
2024-03-28 00:39:04 +0900

    :sparkles: 스크린 타임 API 권한 요청 로직 추가

--- AppLocker/AppLockerApp.swift
+++ AppLocker/AppLockerApp.swift
@@ -6,12 +6,27 @@
 //
 
 import SwiftUI
+import FamilyControls
 
 @main
 struct AppLockerApp: App {
+
+    @StateObject var model = Model.shared
+    let center = AuthorizationCenter.shared
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(model)
+                .onAppear {
+                    Task {
+                        do {
+                            try await center.requestAuthorization(for: .individual)
+                        } catch {
+                            print("Failed to enroll Aniyah with error: \(error)")
+                        }
+                    }
+                }
         }
     }
 }
